054ddc96217c9aeeed027f2774470f2d5f1170b5
kataya <shinji_kataya@esrij.com>
2020-04-27 17:20:08 +0900

    Add the .NET Framework version check.

--- Install_Batchfiles/Pro_250_Install_Full.bat
+++ Install_Batchfiles/Pro_250_Install_Full.bat
@@ -16,8 +16,28 @@ REM デスクトップのインストール
 echo ArcGIS Pro 2.5 のインストールを開始します
 
 
-REM 事前に必要なものをインストール
-REM Proは事前に必要なものは.NET Framework 4.6.1
+REM 事前に必要なものをインストールチェック
+REM Pro 2.5 で事前に必要なものは.NET Framework 4.8
+REM レジストリ値をチェック
+REM reg query "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v "Release"
+REM .NET での Release 値はMSのサイトに記載されている
+REM https://docs.microsoft.com/ja-jp/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+REM 528040(80ea8) - .NET 4.8
+set /A COMPNET = 528040
+echo .NET Framework のバージョンをチェックします...
+FOR /F "tokens=1,2,*" %%I IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v "Release"') DO (
+  REM SET /A VAL = %%K で数値として格納
+  IF "%%I"=="Release" SET /A NETVAL=%%K
+)
+
+IF %NETVAL% geq %COMPNET% (
+  GOTO NETOK
+) else (
+  GOTO NETNG
+)
+
+:NETOK
+REM echo .NET Framework 4.8 を確認しました。インストール可能です。
 
 
 REM ArcGIS Pro 本体のインストール
@@ -52,6 +72,11 @@ REM その他:日本用地図シンボルなどはバッチでは？
 echo 注1)ArcGIS Pro 2.5 用 ST_Geometry ライブラリ と ST_Raster ライブラリ は必要に応じてインストールしてください
 echo 注2)日本用地図シンボル は必要に応じてインストールしてください
 
+:NETNG
+echo 事前条件の .NET Framework 4.8 を確認できないため、インストールを継続できません。
+GOTO EXITEND
+
+:EXITEND
 
 pause
 exit /b
