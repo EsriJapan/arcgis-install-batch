5f98937f8c77bb746464d9bd648e929477c6086f
kataya <shinji_kataya@esrij.com>
2021-02-15 09:58:59 +0900

    Add ArcGIS Pro 2.7 batch

---
+++ Install_Batchfiles/Pro_270_Install_Full.bat
@@ -0,0 +1,92 @@
+@echo off
+REM 
+REM ArcGIS Pro 2.7の一括インストール
+REM 
+
+REM 以下を参考にして編集しました
+REM 参考：https://github.com/StanfordGeospatialCenter/arcgis-install-batch/blob/master/ArcGIS%2010.5.1/ArcGIS_10.5.1_Installation_Full_with_ArcHydro_ArcTutor_DataMaps_Patches.bat
+
+REM バッチでの.NET Framework のバージョンチェックは以下を参考にしました。
+REM 参考:https://www.atmarkit.co.jp/ait/articles/1210/26/news086.html
+
+REM レジストリの値はMSのサイトにあります。
+REM 参考:https://docs.microsoft.com/ja-jp/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+REM   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full
+REM   .NET Framework 4.8 の場合は Release REG_DWORD 値 528040
+
+set __COMPAT_LAYER=RunAsInvoker
+
+REM 相対パスで処理するためバッチファイルの実行ディレクトリを設定
+set curpath=%~dp0
+
+
+REM デスクトップのインストール
+echo ArcGIS Pro 2.7 のインストールを開始します
+
+
+REM 事前に必要なものをインストールチェック
+REM Pro 2.7 で事前に必要なものは.NET Framework 4.8
+REM レジストリ値をチェック
+REM reg query "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v "Release"
+REM .NET での Release 値はMSのサイトに記載されている
+REM https://docs.microsoft.com/ja-jp/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
+REM 528040(80ea8) - .NET 4.8
+set /A COMPNET = 528040
+echo .NET Framework のバージョンをチェックします...
+FOR /F "tokens=1,2,*" %%I IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v "Release"') DO (
+  REM SET /A VAL = %%K で数値として格納
+  IF "%%I"=="Release" SET /A NETVAL=%%K
+)
+
+IF %NETVAL% geq %COMPNET% (
+  GOTO NETOK
+) else (
+  GOTO NETNG
+)
+
+:NETOK
+REM echo .NET Framework 4.8 を確認しました。インストール可能です。
+
+
+REM ArcGIS Pro 本体のインストール
+echo ArcGIS Pro 2.7 本体のインストール... インストールにはしばらく時間がかかります...
+%windir%\System32\msiexec.exe /I "%curpath%\ArcGISPro_Japanese\SetupFiles\ArcGISPro.msi" ALLUSERS=1 /norestart /passive /qb
+
+REM ArcGIS Pro 日本語パックのインストール
+echo ArcGIS Pro 2.7 日本語パック のインストール...
+%windir%\System32\msiexec.exe /I "%curpath%\ArcGISPro_Japanese\SetupFiles\Supplement\ProJapaneseLP.msi" /norestart /passive /qb
+
+
+REM ArcGIS Helpインストール
+echo ArcGIS Pro オフライン ヘルプ のインストール...
+%windir%\System32\msiexec.exe /I "%curpath%\ArcGISProHelp_Japanese\ArcGISProHelp_Japanese.msi" /norestart /passive /qb
+
+
+echo ArcGIS Pro 2.7 のインストール終了
+
+
+
+REM パッチのインストール
+echo パッチのインストール開始
+
+
+echo ArcGIS Pro 2.7 Patch 1 のインストール 
+%windir%\System32\msiexec.exe /p "%curpath%\ArcGISPro_Patches\ArcGIS_Pro_271_176643.msp" /norestart /passive /qb
+
+
+echo パッチのインストール終了
+
+REM その他:ArcGIS Pro 2.0 用 ST_Geometry ライブラリ と ST_Raster ライブラリ
+REM その他:日本用地図シンボルなどはバッチでは？
+echo 注1)ArcGIS Pro 2.7 用 ST_Geometry ライブラリ と ST_Raster ライブラリ は必要に応じてインストールしてください
+echo 注2)日本用地図シンボル は必要に応じてインストールしてください
+
+
+:NETNG
+echo 事前条件の .NET Framework 4.8 を確認できないため、インストールを継続できません。
+GOTO EXITEND
+
+:EXITEND
+
+pause
+exit /b
---
+++ Uninstall_Batchfiles/Pro_270_UnInstall_Full.bat
@@ -0,0 +1,60 @@
+@echo off
+REM ArcGIS Pro 2.7の一括アンインストール
+
+REM MSIファイルのアンインストール コマンド
+REM >msiexec /X <xxxx\xxxx.msi> or mxiexec /X <PruductGUID>
+REM 注1)MSIのGUIDの値は「orca」で *.msiを開き、[Tables] > [Property] > [ProductCode]の値を指定します
+REM 
+REM MSPファイルのアンインストール コマンド
+REM >msiexec /I <ProductGUID> MSIPATCHREMOVE=<PatchGUID>
+REM 注2)mspのGUIDの値は「orca」で *.mspを開き、[View]メニュー > [Summary Information]の画面の
+REM [Patch Code]=<PatchGUID>と[Targets]=<ProductGUID>の値を指定します
+REM 
+
+
+set __COMPAT_LAYER=RunAsInvoker
+
+
+
+REM デスクトップのアンインストール
+echo ArcGIS Pro 2.7 のアンインストールを開始します
+
+
+
+
+REM パッチのアンインストール
+echo パッチのアンインストール開始
+
+echo ArcGIS Pro 2.7 Patch 1 のアンインストール
+%windir%\System32\msiexec.exe /I "{FBBB144A-B4BE-49A0-95C4-1007E3A42FA5}" MSIPATCHREMOVE="{2F94FC7D-896F-45E4-B25B-22570E0357A9}" /norestart /passive /qb
+
+echo パッチのアンインストール終了
+
+
+
+REM ArcGIS Helpアンインストール
+echo ArcGIS Pro オフライン ヘルプ のアンインストール...
+%windir%\System32\msiexec.exe /X "{54A2C55C-CB1C-4495-811B-52C5E03143D5}" /norestart /passive /qb
+
+
+REM ArcGIS Pro 日本語パックのアンインストール
+echo ArcGIS Pro 2.7 日本語パック のアンインストール...
+%windir%\System32\msiexec.exe /X "{F32EE75E-E50E-47DF-8F08-418E97BE9CF8}" /norestart /passive /qb
+
+
+REM ArcGIS Pro 本体のアンインストール
+echo ArcGIS Pro 2.7 本体のアンインストール... アンインストールにはしばらく時間がかかります...
+%windir%\System32\msiexec.exe /X "{FBBB144A-B4BE-49A0-95C4-1007E3A42FA5}" /norestart /passive /qb
+
+
+echo ArcGIS Pro 2.7 のアンインストール終了
+
+
+REM その他:ArcGIS Pro 2.3 用 ST_Geometry ライブラリ と ST_Raster ライブラリ
+REM その他:日本用地図シンボル
+echo 注1)ArcGIS Pro 2.7 用 ST_Geometry ライブラリ と ST_Raster ライブラリ は必要に応じてアンインストールしてください
+echo 注2)日本用地図シンボル は必要に応じてアンインストールしてください
+
+
+pause
+exit /b
